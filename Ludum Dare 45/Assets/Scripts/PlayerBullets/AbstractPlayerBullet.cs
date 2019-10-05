a30dc2c593cb895c11979348d5c4b3cd8cf80dc6
J4AAceman <j4a.aceman@gmail.com>
2019-10-04 23:11:11 -0500

    Movement works and is constrained to the screen, generic weapon framework started

---
+++ Ludum Dare 45/Assets/Scripts/PlayerBullets/AbstractPlayerBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractPlayerBullet : MonoBehaviour
+{
+    protected const float BulletLifetime = 2.0f;
+    protected Vector2 StartingVelocity = Vector2.up * 10;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        Destroy(gameObject, BulletLifetime);
+        this.GetComponent<Rigidbody2D>().velocity = StartingVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ Ludum Dare 45/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MaxPlayerSpeed = 1f;
+    public GameObject PlayerShip;
+    public List<AbstractPlayerWeapon> PlayerWeaponList;
+
+    private Transform playerShipTransform;
+    //private Vector2 velocity;
+    private Rigidbody2D playerShipRigidBody;
+
+    private void Awake()
+    {
+        //velocity = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerShip)
+        {
+            playerShipTransform = PlayerShip.transform;
+            playerShipRigidBody = PlayerShip.GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+            Debug.LogError("Missing PlayerShip");
+        }
+
+        if (!playerShipTransform)
+        {
+            Debug.LogError("Missing PlayerShip Transform");
+        }
+        if(!playerShipRigidBody)
+        {
+            Debug.LogError("Missing PlayerShip Rigidbody2D");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool shouldFire = Input.GetAxis("Fire1") > 0f;
+        Vector2 velocity = new Vector2(horizontal, vertical);
+
+        if(velocity.magnitude > 1)
+        {
+            velocity.Normalize();
+        }
+
+        playerShipRigidBody.velocity = velocity * MaxPlayerSpeed;
+
+        foreach(var w in PlayerWeaponList)
+        {
+            w.ShouldFire = shouldFire;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
